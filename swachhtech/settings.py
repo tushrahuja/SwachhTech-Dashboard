5575ddbbad42d64d0b71eca3d224e539f1a26c3a
tushrahuja <ahuja.tushar.m@gmail.com>
2024-10-02 17:03:34 +0530

    Removed database credentials

--- swachhtech/settings.py
+++ swachhtech/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tiyh%62=dj2la0bitj#r!@%ma*a7s76%6fc+!%dks-o-3_w()i'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,6 +77,11 @@ WSGI_APPLICATION = 'swachhtech.wsgi.application'
 DATABASES = {
     'default':  {
         "ENGINE": "django.db.backends.mysql",
+        "NAME": config("DATABASE_NAME"),
+        "USER": config("DATABASE_USER"),
+        "PASSWORD": config("DATABASE_PASSWORD"),
+        "HOST": config("DATABASE_HOST"),
+        "PORT": config("DATABASE_PORT"),
     }
 }
 
